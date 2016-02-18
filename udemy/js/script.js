2546e0d2eb46eff1b97748c9e91088fa9ae72990
Samuel <samuel.9401@gmail.com>
2016-02-18 16:06:20 -0600

    Update script.js

--- udemy/js/script.js
+++ udemy/js/script.js
@@ -43,39 +43,42 @@ var users = [
     },
 ];
 
-
 window.addEventListener('load', function() {
-    function search(){
-            var hobbyField = document.getElementById('hobby');
+    
+    function search() {
+        
+        //get hobby
+        var hobbyField = document.getElementById('hobby');
         var hobby = hobbyField.value;
         
+        //get gender
         var genderField = document.getElementById('gender');
-        var gender = genderField.options[genderField.selectedIndex].value;
+        var selected = genderField.selectedIndex;
+        var gender = genderField.options[selected].value;
         console.log(gender);
         
         var resultsHTML = '';
         var numUsers = users.length;
         
-        for(var i=0; i<numUsers; i++){
-            if (gender== 'A' || gender == users[i].gender) {
-                if (hobby==''||hobby==users[i].hobby) {
-            resultsHTML +=  '<div class="person-row">\
-                    <img src="images/'+users[i].avatar+'" />\
-                    <div class="person-info">\
-                        <div>'+ users[i].name+'</div>\
-                        <div>'+users[i].hobby+'</div>\
-                    </div> \
-                    <button>Add as friend</button>\
-                </div>';
-            };
+        for(var i = 0; i < numUsers; i++) {
+            
+            if (gender == 'A' || gender == users[i].gender) {             
+                if (hobby == '' || hobby == users[i].hobby) {
+                    resultsHTML += '<div class="person-row">\
+                                   <img src="images/' + users[i].avatar + '" />\
+                                   <div class="person-info">\
+                                   <div>' + users[i].name + '</div>\
+                                   <div>' + users[i].hobby + '</div></div>\
+                                    <button>Add as friend</button></div>';
+                }
+            }
         }
         
-        results.innerHTML = resultsHTML;
-    }
-    }
-
-    console.log('se ha cargado la pagina');
+        results.innerHTML = resultsHTML;  
+    };
+    
     var results = document.getElementById('results');
     var searchBtn = document.getElementById('searchBtn');
     searchBtn.addEventListener('click', search);
+    search();
 });
